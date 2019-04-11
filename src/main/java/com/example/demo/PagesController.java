4a4def8607d5042af99902a0d382ecee89932595
Sanjay Shivraj <shivraj.sanjay@gmail.com>
2019-04-11 11:46:18 -0400

    added 'math/calculate' end point with tested operations (add, subtract, multiply, delete)

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,5 +15,26 @@ public class PagesController {
     @GetMapping("/math/pi")
     public String getPi() {
         return "3.141592653589793";
+    }
+
+    @GetMapping("/math/calculate")
+    public String getCalculation(@RequestParam(value="operation", defaultValue = "add")String operation,
+                                 @RequestParam(required = true) int x, @RequestParam(required = true) int y) {
+        switch (operation){
+            case "add":
+                return x + " + " + y + " = " + (x + y);
+
+            case "multiply":
+                return x + " * " + y + " = " + (x * y);
+
+            case "subtract":
+                return x + " - " + y + " = " + (x - y);
+
+            case "divide":
+                return x + " / " + y + " = " + (x / y);
+
+        }
+
+        return "";
     }
 }
